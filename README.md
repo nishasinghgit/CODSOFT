b1a2062a349bb06537e5ce02148a005575e82f18
Nakul Gupta <nishasingh12a2@gmail.com>
2023-08-13 17:12:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## ***Codesoft Intership Files By Nisha Singh***
+
+**I have choosed "LEVEL 1" in Web Development internship.**
---
+++ codsoft_3/script.js
@@ -0,0 +1,19 @@
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+
+function equals() {
+    var txt = document.form.textview.value;
+    if (txt)
+        document.form.textview.value = eval(txt);
+
+}
+
+function clean() {
+    document.form.textview.value = "";
+}
+
+function back() {
+    var txt = document.form.textview.value;
+    document.form.textview.value = txt.substring(0, txt.length - 1);
+}
